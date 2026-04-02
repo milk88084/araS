596cf4e114268673891d2251253b3be06a6035de
Max <milk88084@gmail.com>
2026-04-02 16:12:52 +0800

    chore: remove posts from api

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import healthRoutes from "./health.js";
-import postsRoutes from "./posts.js";
 
 const router = Router();
 
 router.use("/health", healthRoutes);
-router.use("/posts", postsRoutes);
 
 export default router;
